3e56a2f1ececd0a0ee2c7eee41fd4331578534c2
Dollars <aomsup5185@gmail.com>
2024-02-28 15:58:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express')
-const mysql = require('mysql')
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
+
+dotenv.config();
+app.use(bodyParser.json())
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: process.env.DBHOST,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+})
+
+    db.connect((err) => {
+        if(err){
+            throw err
+        } 
+        console.log("Database already connected");
+    })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
